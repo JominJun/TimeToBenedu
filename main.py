07b255eabb036e2365b70d3fbb7c36717028e128
JominJun <mj3029327@naver.com>
2020-09-09 14:43:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time, win32con, win32api, win32gui, ctypes
 
 # 채팅방 이름
-kakaoRoomName = ["고1 쌤없 반톡", "1-5반 (선생님)"]
+kakaoRoomName = ["고1 쌤없 반톡"]
 
 _user32 = ctypes.WinDLL("user32")
 PostMessage = win32api.PostMessage
@@ -35,8 +35,8 @@ while True:
     end_hour = 15 if day == "Wed" else 16 # 수요일은 15시까지, 다른 날은 16시까지
     ok_hour = [i for i in range(start_hour, end_hour+1, term_hour)] # 1시간마다 전송
     send_min = 50 if hour < 13 else 40 # 점심 전까지는 50분에, 이후에는 40분에 안내
-    message = "📢 현재 시간 {0}시 {1}분을 지나가고 있습니다.\n" \
-              "1학년 5반 학생들은 금일 16시 40분까지 [베네듀 일일 과제]를 신속히 완료하시기 바랍니다.".format(hour, minute)
+    message = "📢 [Bot] 현재 시간 {0}시 {1}분을 지나가고 있습니다.\n" \
+              "1학년 5반 학생들은 금일 {2}시 40분까지 [베네듀 일일 과제]를 신속히 완료하시기 바랍니다.".format(hour, minute, end_hour)
 
     for room in kakaoRoomName:
         if(day in ok_day):
